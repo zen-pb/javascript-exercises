5280ab091a5586117bdc782089fb111e1244ad0e
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-28 00:34:49 +0800

    Add existing formula for conversion to code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((5 / 9) * (fahrenheit - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((9 / 5) * celsius + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
