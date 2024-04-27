cae9bd1e54b357ce70506b19409e96dfc9c89f57
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-28 00:12:16 +0800

    Use an existing formula in finding out the sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sumOfNumbers = undefined;
 
+  if (
+    Number.isInteger(num1) &&
+    Number.isInteger(num2) &&
+    num1 >= 0 &&
+    num2 >= 0
+  ) {
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+
+    sumOfNumbers = ((start + end) * (end - start + 1)) / 2;
+  } else {
+    return "ERROR";
+  }
+
+  return sumOfNumbers;
 };
 
 // Do not edit below this line
