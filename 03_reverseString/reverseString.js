d83c3d0282d1f5c02be26ab4b75bdfff98a43ca1
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-27 23:32:55 +0800

    Add comment explaining how the code works

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  /* The string gets split into an array of characters, 
+     the array of characters is reversed, 
+     then concatenate all characters into a single string.
+  */
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
