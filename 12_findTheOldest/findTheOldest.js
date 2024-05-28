2568ebab473c597b3a4b32cc6f0b15aeb453f69f
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-28 23:04:22 +0800

    Finish the findTheOldest function, but failed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge =
+      (!oldest.yearOfDeath ? new Date().getFullYear() : oldest.yearOfDeath) -
+      oldest.yearOfBirth;
+    const currentAge =
+      (!current.yearOfDeath ? new Date().getFullYear() : current.yearOfDeath) -
+      current.yearOfBirth;
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
