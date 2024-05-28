5e9cfa282bec9e9c103d7d96718985a7501a7f0d
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-28 21:39:18 +0800

    Finish the palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  const punctSpaces = [" ", ".", ",", "!", "?"];
 
+  phrase = phrase.toLowerCase().split("");
+
+  for (let i = 0; i < phrase.length; i++) {
+    let val = phrase[i];
+
+    for (let punctSpace of punctSpaces) {
+      if (val === punctSpace) {
+        phrase.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return phrase.join("") === phrase.reverse().join("") ? true : false;
 };
 
 // Do not edit below this line
