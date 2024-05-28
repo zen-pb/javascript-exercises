9948920d7488f1495d3d064544561bbeea470122
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-28 22:01:21 +0800

    Finish the fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (!isNaN(Number(num)) && num >= 0) {
+    num = Number(num);
+    if (num <= 1) {
+      return num;
+    } else {
+      return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+  }
+  return "OOPS";
 };
 
 // Do not edit below this line
