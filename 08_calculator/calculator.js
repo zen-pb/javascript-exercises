e9abc935e95f81bafa894e8b731534732b73d121
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-28 20:32:59 +0800

    Finish the factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,8 @@ const power = function(num1, num2) {
 	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return num === 0 || num === 1 ? 1 : num * factorial(num - 1);
 };
 
 // Do not edit below this line
